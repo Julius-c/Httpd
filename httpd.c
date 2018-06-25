2e968c839df27bc7c0e6a6b233fac921f6874b2f
tracer <tracer@tracer.org>
2018-06-25 12:58:43 +0800

    >  compile cccc Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  return 0;
+    if(argc == 1) {
+        printf("\033[1;33mUsage: ./httpd [OPTION]... ./DIR\033[0m\n]]");
+        exit(EXIT_SUCCESS);
+    }
+    return 0;
 }
