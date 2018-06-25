17c19893b5697bce9a186ab5de85cb3f021d5a79
tracer <tracer@tracer.org>
2018-06-25 13:04:28 +0800

    >  compile cccc Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -4,10 +4,14 @@
 #include <unistd.h>
 #include <assert.h>
 
+unsigned long PORT;
+
 int main(int argc, char *argv[]) {
     if(argc == 1) {
         printf("\033[1;33mUsage: ./httpd [OPTION]... ./DIR\033[0m\n");
+        printf("\033[1;33mSample: ./httpd --port 8000 ./site\033[0m\n");
         exit(EXIT_SUCCESS);
     }
+    if()
     return 0;
 }
