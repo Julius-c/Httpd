49e7dcb30286e877bceac337c010299873d066c5
tracer <tracer@tracer.org>
2018-06-25 14:57:29 +0800

    >  compile cccc Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -4,23 +4,55 @@
 #include <unistd.h>
 #include <assert.h>
 #include <dirent.h>
+#include <signal.h>
+#include <pthread.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int port = 8000; //default
-void server(int port, char *dir) {
-   DIR *site = NULL;
-   struct dirent *entry;
-   char path[128];
-   sscanf(dir, "./%s", path);
-   assert((site = opendir(path)) != NULL);
-   while((entry = readdir(site)) != NULL) {
-       printf("%s\n", entry->d_name);
-       if(entry->d_type & DT_DIR) {
-           if(strcmp(entry->d_name, ".") == 0
-              || strcmp(entry->d_name, "..") == 0)
-               continue;
-       }
-   }
+int servfd = -1;
+int servport = 8000; //default
+
+void sigint_handler(int signum) {
+    printf("Keyboard Interrupt Detected, Close Server\n");
+    close(servfd);
+    exit(EXIT_SUCCESS);
+}
+
+void server(int servport, char *dir) {
+    servfd = socket(AF_INET, SOCK_STREAM, 0);
+    signal(SIGINT, sigint_handler);
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_faminy = AF_INET;
+    servaddr.sin_port = htons(servport);
+    servaddr.sin_addr.s_addr = htonl(INADD_ANY);
+    bind(servfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    listen(servfd, 50);
+    struct sockaddr_in cliend_addr;
+    socklen_t length = sizeof(client_addr);
+    int conn = -1;
+    while((conn = accept(servfd, (struct sockaddr *)&client_addr, &length)) != -1) {
+        const char response[] = "Hello World!";
+        int len = write(conn, response, sizeof(response));
+        len ++;
+        close(conn);
+    }
+
+    DIR *site = NULL;
+    struct dirent *entry;
+    char path[128];
+    sscanf(dir, "./%s", path);
+    assert((site = opendir(path)) != NULL);
+    while((entry = readdir(site)) != NULL) {
+        printf("%s\n", entry->d_name);
+        if(entry->d_type & DT_DIR) {
+            if(strcmp(entry->d_name, ".") == 0
+                || strcmp(entry->d_name, "..") == 0)
+                continue;
+        }
+    }
 }
 
 static struct{
@@ -41,8 +73,8 @@ int main(int argc, char *argv[]) {
     if(argc >= 2) {
         if( (strcmp(argv[1], "-p") == 0 ||
             strcmp(argv[1], "--port") == 0) && argc == 4) {
-            port = atoi(argv[2]);
-            server(port, argv[3]);
+            servport = atoi(argv[2]);
+            server(servport, argv[3]);
         }else if( (strcmp(argv[1], "-h") == 0 ||
                   strcmp(argv[1], "--help") == 0) && argc == 2) {
             for(int i = 0; i < NR_CMD; i ++)
