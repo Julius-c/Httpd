7a150d9ba42743302555b7fbf2915ea88be56314
tracer <tracer@tracer.org>
2018-06-25 19:04:08 +0800

    >  compile cccc Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -41,7 +41,43 @@ void server(int servport, char *dir) {
     listen(servfd, 50);
     static char response[1 << 20];
 //    sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s\r\n", filesize, index);
-    sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello World!\r\n");
+    sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\n<html>
+  <Head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="static/bootstrap.min.css"/>
+    <link rel="stylesheet" href="static/wiki.css"/>
+    <title>Example Site</title>
+  </Head>
+  <body>
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-header">
+        <div class="navbar-brand">
+          <a href="/">Example Site</a>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-left"> 
+          <li><a href="about.html">关于我们</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <center>
+      <div class="article-container">
+        <div class="article">
+          <h1>It Works!</h1>
+          <p>如果你能正确看到这段文字，说明浏览器工作正常。</p>
+        </div>
+      </div>
+    </center>
+
+    <div class="footer-bottom">
+      <center>
+      </center>
+    </div>
+  </body>
+</html>
+");
 
     while((conn = accept(servfd, (struct sockaddr *)&client_addr, &length)) != -1) {
 /*		const char response[] = 
@@ -79,7 +115,7 @@ int main(int argc, char *argv[]) {
     assert((site = opendir(path)) != NULL);
     while((entry = readdir(site)) != NULL) {
         if(strcmp(entry->d_name, "index.html") == 0) {
-   /*         sprintf(pwd, "%s/%s/%s", pwd, path, entry->d_name);
+/*            sprintf(pwd, "%s/%s/%s", pwd, path, entry->d_name);
             FILE *fp = fopen(pwd, "r");
             fseek(fp, 0L, SEEK_END);
             int filesize = ftell(fp);
