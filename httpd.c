2f9cc8d8cdfebfa5e2c5fdcb3460de160132eb3c
tracer <tracer@tracer.org>
2018-06-25 14:14:29 +0800

    >  compile cccc Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -10,7 +10,7 @@ void server(int port, char *dir) {
    DIR *site = NULL;
    struct dirent *entry;
    char path[512];
-   sscanf(dir, "%s", path);
+   sscanf(dir, "%[a-z]", path);
    printf("%s %s\n", dir, path);
 }
 
