9210ad73939c468f34789f81e7fec399b6197425
tracer <tracer@tracer.org>
2018-06-25 14:40:26 +0800

    >  compile cccc Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- httpd.c
+++ httpd.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <dirent.h>
+#include <sys/types.h>
 
 int port = 8000; //default
 void server(int port, char *dir) {
@@ -14,6 +15,11 @@ void server(int port, char *dir) {
    assert((site = opendir(path)) != NULL);
    while((entry = readdir(site)) != NULL) {
        printf("%s\n", entry->d_name);
+       if(entry->d_type & DT_DIR) {
+           if(strcmp(entry->d_name, ".") == 0
+              || strcmp(entry->d_name, "..") == 0)
+               continue;
+       }
    }
 }
 
